085922983d72844daa15ec690689b2e69a377c6f
Martín Calderón <martincalderon92@outlook.com>
2021-09-15 18:19:38 -0300

    :sparkles: Add JavaScript validation for inputs.

---
+++ js/script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+// Declaration of Variables
+
+let form = document.getElementById('Form');
+let errorsList = document.getElementById('Errors List');
+let errors = [];
+
+// Custom submit logic
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	errors = [];
+	let formItems = form.elements;
+	let password = '';
+
+	for (let i = 0; i < formItems.length - 1; i++) {
+		if (formItems[i].value.length === 0) {
+			errors.push(`<li>${formItems[i].name} is required.</li>`);
+		} else {
+			formItems[i].type === 'text' &&
+				isInvalid(formItems[i], 'text') &&
+				errors.push(`<li>${formItems[i].name} must have letters only.</li>`);
+
+			formItems[i].type === 'email' &&
+				isInvalid(formItems[i], 'email') &&
+				errors.push(`<li>${formItems[i].name} has an invalid format.</li>`);
+
+			if (formItems[i].id === 'Password') {
+				password = formItems[i].value;
+			}
+
+			formItems[i].id === 'Repeat Password' &&
+				password !== formItems[i].value &&
+				errors.push(`<li>Passwords do not match.</li>`);
+
+			formItems[i].type === 'tel' &&
+				isInvalid(formItems[i], 'tel') &&
+				errors.push(`<li>${formItems[i].name} has an invalid format.</li>`);
+
+			formItems[i].type === 'number' &&
+				isInvalid(formItems[i], 'age') &&
+				errors.push(
+					`<li>${formItems[i].name} must be a number between 1 and 99.</li>`
+				);
+
+			formItems[i].type === 'url' &&
+				isInvalid(formItems[i], 'url') &&
+				errors.push(`<li>${formItems[i].name} has an invalid format.</li>`);
+
+			formItems[i].type === 'checkbox' &&
+				!formItems[i].checked &&
+				errors.push(`<li>Accept the ${formItems[i].name} is required.</li>`);
+		}
+	}
+
+	if (errors.length > 0) {
+		errorsList.innerHTML = errors.join('');
+	} else {
+		errorsList.innerHTML = '';
+		console.log('Submit');
+	}
+});
+
+// Functions
+
+function isInvalid(str, type) {
+	let regexTypes = {
+		text: /^[a-zA-Z]+$/,
+		email: /\S+@\S+\.\S+/,
+		tel: /^[0-9]{4}-[0-9]{4}$/,
+		age: /^[0-9]{2}$/,
+		url: /\S+\.\S+\.\S+/,
+	};
+
+	let regex = regexTypes[type];
+
+	if (!str.value.match(regex)) {
+		return true;
+	}
+
+	return false;
+}
+
+function onlyNumbers() {
+	let e = window.event;
+	let key = e.keyCode;
+
+	if ((key >= 65 && key <= 90) || key == 192) {
+		e.preventDefault && e.preventDefault();
+	}
+}
