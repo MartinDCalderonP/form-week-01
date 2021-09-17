cc1277b6579e088bb6ed293f2ace2d7e178d0e0f
Martín Calderón <martincalderon92@outlook.com>
2021-09-17 17:16:17 -0300

    :art: Improve show modal function.

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,13 @@
 let overlay = document.getElementById('overlay');
 let closeIcon = document.getElementById('closeIcon');
-let modal = document.getElementById('modal');
 
 overlay.addEventListener('click', hideModal);
-
 closeIcon.addEventListener('click', hideModal);
 
 function hideModal() {
 	overlay.style.display = 'none';
-	modal.style.display = 'none';
 }
 
 export function showModal() {
 	overlay.style.display = 'block';
-	modal.style.display = 'block';
 }
