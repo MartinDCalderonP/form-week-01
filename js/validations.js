1640e96644d7c507ae150069d504ec3612927c62
Martín Calderón <martincalderon92@outlook.com>
2021-09-17 23:29:45 -0300

    :sparkles: Add error & succes icons and refactor main script & validations.

--- js/validations.js
+++ js/validations.js
@@ -1,6 +1,10 @@
+let firstName = document.getElementById('First Name');
+let lastName = document.getElementById('Last Name');
 let ageInput = document.getElementById('Age');
 let phoneInput = document.getElementById('Phone Number');
 
+firstName.addEventListener('keydown', avoidNumbers);
+lastName.addEventListener('keydown', avoidNumbers);
 ageInput.addEventListener('keydown', avoidLetters);
 phoneInput.addEventListener('keydown', avoidLetters);
 
@@ -12,16 +16,59 @@ function avoidLetters(e) {
 	}
 }
 
-export function isInvalid(str, type) {
+function avoidNumbers(e) {
+	let key = e.key.charCodeAt();
+
+	if (key >= 48 && key <= 57) {
+		e.preventDefault();
+	}
+}
+
+let currentPassword = '';
+
+export default function isValidValue(input) {
+	if (input.value.length === 0) {
+		return `<li>${input.name} is required.</li>`;
+	}
+
+	if (input.id === 'Password') {
+		currentPassword = input.value;
+	}
+
+	if (input.id === 'Repeat Password') {
+		if (currentPassword !== input.value) {
+			return `<li>Passwords do not match.</li>`;
+		}
+
+		return true;
+	}
+
+	if (input.type === 'checkbox' && !input.checked) {
+		return `<li>Accept the ${input.name} is required.</li>`;
+	}
+
 	let regexTypes = {
 		text: /^[a-zA-Z]+$/,
 		email: /\S+@\S+\.\S+/,
 		tel: /^[0-9]{4}-[0-9]{4}$/,
-		age: /^[0-9]{2}$/,
+		number: /^[0-9]{2}$/,
 		url: /\S+\.\S+\.\S+/,
 	};
 
-	let regex = regexTypes[type];
+	let messagesTypes = {
+		text: `<li>${input.name} must have letters only.</li>`,
+		email: `<li>${input.name} format must be "example@example.com".</li>`,
+		tel: `<li>${input.name} format must be "XXXX-XXXX".</li>`,
+		number: `<li>${input.name} must be a number between 1 and 99.</li>`,
+		url: `<li>${input.name} format must be "http://www.example.com".</li>`,
+	};
 
-	return !str.value.match(regex) ? true : false;
+	let regex = regexTypes[input.type];
+	let message = messagesTypes[input.type];
+
+	if (input.value.match(regex)) {
+		return true;
+	} else {
+		return message;
+	}
 }
