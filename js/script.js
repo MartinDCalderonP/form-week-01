76de91919e36b3f462dfcac6ce3c03797be2a53f
Martín Calderón <martincalderon92@outlook.com>
2021-09-17 23:26:51 -0300

    ✨ Add icons of error and success and refactor main script and validations.

--- js/script.js
+++ js/script.js
@@ -1,72 +1,56 @@
 'use strict';
-import { isInvalid } from './validations.js';
+import isValidValue from './validations.js';
 import './konamiCode.js';
 import './rangeChanger.js';
 
 let form = document.getElementById('Form');
+let formItems = form.elements;
 let errorsList = document.getElementById('Errors List');
-let errors = [];
+let errorsArray = [];
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', customSubmit);
+
+function customSubmit(e) {
 	e.preventDefault();
 
-	errors = [];
-	let formItems = form.elements;
-	let password = '';
+	errorsArray = [];
 
 	for (let i = 0; i < formItems.length - 1; i++) {
-		if (formItems[i].value.length === 0) {
-			errors.push(`<li>${formItems[i].name} is required.</li>`);
-		} else {
-			formItems[i].type === 'text' &&
-				isInvalid(formItems[i], 'text') &&
-				errors.push(`<li>${formItems[i].name} must have letters only.</li>`);
-
-			formItems[i].type === 'email' &&
-				isInvalid(formItems[i], 'email') &&
-				errors.push(`<li>${formItems[i].name} has an invalid format.</li>`);
-
-			if (formItems[i].id === 'Password') {
-				password = formItems[i].value;
-			}
-
-			formItems[i].id === 'Repeat Password' &&
-				password !== formItems[i].value &&
-				errors.push(`<li>Passwords do not match.</li>`);
-
-			formItems[i].type === 'tel' &&
-				isInvalid(formItems[i], 'tel') &&
-				errors.push(`<li>${formItems[i].name} has an invalid format.</li>`);
-
-			formItems[i].type === 'number' &&
-				isInvalid(formItems[i], 'age') &&
-				errors.push(
-					`<li>${formItems[i].name} must be a number between 1 and 99.</li>`
-				);
-
-			formItems[i].type === 'url' &&
-				isInvalid(formItems[i], 'url') &&
-				errors.push(`<li>${formItems[i].name} has an invalid format.</li>`);
-
-			formItems[i].type === 'checkbox' &&
-				!formItems[i].checked &&
-				errors.push(`<li>Accept the ${formItems[i].name} is required.</li>`);
+		if (formItems[i].type !== 'range') {
+			typeof isValidValue(formItems[i]) !== 'string'
+				? inputSuccess(formItems[i])
+				: inputError(formItems[i], isValidValue(formItems[i]));
 		}
 	}
 
-	if (errors.length > 0) {
-		errorsList.innerHTML = errors.join('');
-	} else {
-		errorsList.innerHTML = '';
+	errorsArray.length > 0
+		? (errorsList.innerHTML = errorsArray.join(''))
+		: valuesToConsoleTable();
+}
 
-		let tableItems = {};
-
-		for (let i = 0; i < formItems.length - 1; i++) {
-			formItems[i].type !== 'checkbox'
-				? (tableItems[formItems[i].name] = [formItems[i].value])
-				: (tableItems[formItems[i].name] = [formItems[i].checked]);
-		}
-
-		console.table(tableItems);
+function inputSuccess(input) {
+	if (input.type !== 'checkbox') {
+		input.parentElement.className = 'inputDiv success';
 	}
-});
+}
+
+function inputError(input, message) {
+	errorsArray.push(message);
+	if (input.type !== 'checkbox') {
+		input.parentElement.className = 'inputDiv error';
+	}
+}
+
+function valuesToConsoleTable() {
+	errorsList.innerHTML = '';
+
+	let tableItems = {};
+
+	for (let i = 0; i < formItems.length - 1; i++) {
+		formItems[i].type !== 'checkbox'
+			? (tableItems[formItems[i].name] = [formItems[i].value])
+			: (tableItems[formItems[i].name] = [formItems[i].checked]);
+	}
+
+	console.table(tableItems);
+}
